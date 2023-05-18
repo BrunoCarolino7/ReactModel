b4602db615d663870baccbf0ddf04d2aea416f9f
Bruno Carolino <lopesb073@gmail.com>
2023-05-18 11:54:03 -0300

    initialCommit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import ReactModal from "react-modal";
+
+ReactModal.setAppElement("#root");
+
+export function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
